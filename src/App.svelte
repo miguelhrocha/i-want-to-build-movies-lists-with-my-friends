f95c4c9b765579629ed7e0c6be4398af07b91c7a
Miguel Hernández <miguel.hrocha@gmail.com>
2020-08-06 23:18:34 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<style type="text/postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
+
+<main>
+	<h1 class="bg-blue-500">Hello {name}!</h1>
+	<p class="bg-blue-300">Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
